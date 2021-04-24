f88b3f9921ebc9df2e75204ea82edd59fac37800
Leonardo Arabe Castedo <a2020122243@estudiantes.upsa.edu.bo>
2021-04-24 09:55:00 -0400

    Se incorporó el modelo

---
+++ Sol_Parcial2/Parcial2/Models/Friend.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Parcial2.Models
+{
+    public class Friend
+    {
+        [Key]
+        public int FriendId { get; set; }
+        [Required]
+        [StringLength(30,MinimumLength =3,ErrorMessage ="Ingrese entre 3 a 30 caracteres")]
+        public string Name { get; set; }
+        public string NickName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+
+
+
+    }
+}
